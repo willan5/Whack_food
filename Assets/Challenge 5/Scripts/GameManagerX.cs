c984f4ac17ece41e19df7bc62eb28a2c7982081b
willan <kwanjehia@gmail.com>
2024-10-22 03:45:45 +0300

    final

---
+++ Assets/Challenge 5/Scripts/GameManagerX.cs
@@ -0,0 +1,119 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class GameManagerX : MonoBehaviour
+{
+    public TextMeshProUGUI scoreText;
+    public TextMeshProUGUI gameOverText;
+    public GameObject titleScreen;
+    public Button restartButton;
+    public TextMeshProUGUI timer;
+
+    public List<GameObject> targetPrefabs;
+
+    private int score;
+    private float spawnRate = 1.5f;
+    public bool isGameActive;
+    private  int timeCount;
+
+    private float spaceBetweenSquares = 2.5f; 
+    private float minValueX = -3.75f; //  x value of the center of the left-most square
+    private float minValueY = -3.75f; //  y value of the center of the bottom-most square
+    
+    
+    // Start the game, remove title screen, reset score, and adjust spawnRate based on difficulty button clicked
+    public void StartGame(int difficulty)
+    {
+        spawnRate /= difficulty;
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        StartCoroutine(CountDownTimer());
+        score = 0;
+        timeCount = 60;
+        UpdateScore(0);
+        UpdateTimer();
+        titleScreen.SetActive(false);
+    }
+
+    // While game is active spawn a random target
+    IEnumerator SpawnTarget()
+    {
+        while (isGameActive)
+        {
+            yield return new WaitForSeconds(spawnRate);
+            int index = Random.Range(0, targetPrefabs.Count);
+
+            if (isGameActive)
+            {
+                Instantiate(targetPrefabs[index], RandomSpawnPosition(), targetPrefabs[index].transform.rotation);
+            }
+            
+        }
+    }
+    //Enumerator for count down
+    IEnumerator CountDownTimer()
+    {
+        timeCount = 60;
+        Debug.Log("Countdown started.");
+        while (isGameActive && timeCount > 0)
+        {
+            Debug.Log("In countdown loop: timeCount = " + timeCount);
+            yield return new WaitForSeconds(1);
+            timeCount -= 1;
+            UpdateTimer();
+
+            if (timeCount ==0)
+            {
+                GameOver();
+            }
+
+        }
+    }
+    // Generate a random spawn position based on a random index from 0 to 3
+    Vector3 RandomSpawnPosition()
+    {
+        float spawnPosX = minValueX + (RandomSquareIndex() * spaceBetweenSquares);
+        float spawnPosY = minValueY + (RandomSquareIndex() * spaceBetweenSquares);
+
+        Vector3 spawnPosition = new Vector3(spawnPosX, spawnPosY, 0);
+        return spawnPosition;
+
+    }
+
+    // Generates random square index from 0 to 3, which determines which square the target will appear in
+    int RandomSquareIndex()
+    {
+        return Random.Range(0, 4);
+    }
+
+    // Update score with value from target clicked
+    public void UpdateScore(int scoreToAdd)
+    {
+        score += scoreToAdd;
+        scoreText.text = "score : " + score;
+    }
+    public void UpdateTimer()
+    {
+        timer.text = "Timer : " + timeCount;
+        Debug.Log("Time Remaining: " + timeCount);
+
+    }
+    // Stop game, bring up game over text and restart button
+    public void GameOver()
+    {
+        gameOverText.gameObject.SetActive(true);
+        restartButton.gameObject.SetActive(true);
+        isGameActive = false;
+    }
+
+    // Restart game by reloading the scene
+    public void RestartGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+
+}
